54184b11d2ff62a2f3e889c92206f1e0fd80419e
João Pedro Alves <jotalmeida007@hotmail.com>
2020-09-12 08:05:30 -0300

    Add css styles and connect module 3 api

--- module_4/src/App.js
+++ module_4/src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
+import api from './services/api';
+import './App.css';
 
 function App() {
   const [projects, setProjects] = useState([
@@ -7,24 +9,30 @@ function App() {
     'Front-end web',
   ]);
 
+  useEffect(() => {
+    api.get('/projects').then((response) => {
+      console.log(response);
+    });
+  }, []);
+
   function handleAddProject() {
     // projects.push(`Novo projeto ${Date.now()}`);
 
-    setProjects(...projects, `Novo projeto ${Date.now()}`);
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
     console.log(projects);
   }
   return (
     <>
       <Header title="Projects" />
+      <button type="button" onClick={handleAddProject}>
+        {' '}
+        Adicionar Projeto
+      </button>
       <ul>
         {projects.map((project) => (
           <li key={project}>{project}</li>
         ))}
       </ul>
-
-      <button type="button" onClick={handleAddProject}>
-        Adicionar Projeto
-      </button>
     </>
   );
 }
