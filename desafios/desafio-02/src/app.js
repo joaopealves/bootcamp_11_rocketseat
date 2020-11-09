22b3d6dfde01e5aa4a38eac55f0ef4b68464aa16
João Pedro Alves <jotalmeida007@hotmail.com>
2020-11-09 14:18:44 -0300

    Update readme and add likes routes

--- desafios/desafio-02/src/app.js
+++ desafios/desafio-02/src/app.js
@@ -32,7 +32,7 @@ app.post('/repositories', (request, response) => {
 
 app.put('/repositories/:id', (request, response) => {
   const { id } = request.params;
-  const { title, url, techs, likes } = request.body;
+  const { title, url, techs } = request.body;
 
   const repositorieIndex = repositories.findIndex(
     (repositories) => repositories.id == id
@@ -41,7 +41,7 @@ app.put('/repositories/:id', (request, response) => {
   if (repositorieIndex < 0) {
     return response.status(400).json({ error: 'repository not found' });
   }
-  const repository = { id, title, url, techs, likes };
+  const repository = { id, title, url, techs };
 
   repositories[repositorieIndex] = repository;
 
@@ -66,6 +66,24 @@ app.delete('/repositories/:id', (request, response) => {
 
 app.post('/repositories/:id/like', (request, response) => {
   // TODO
+});
+
+app.put('/repositories/:id/likes', (request, response) => {
+  const { id } = request.params;
+  const { likes } = request.body;
+
+  const repositorieIndex = repositories.findIndex(
+    (repositories) => repositories.id == id
+  );
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'repository not found' });
+  }
+  const repository = { id, likes };
+
+  repositories[repositorieIndex] = repository;
+
+  return response.json(repository);
 });
 
 console.log('Backend Started :3');
